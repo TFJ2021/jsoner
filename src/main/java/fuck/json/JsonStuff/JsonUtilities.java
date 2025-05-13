f6c98a65267317bbe5286d3ff67f19201aee4171
TFJ_5183 <TFJ2009@GMX.DE>
2025-05-13 20:48:36 +0200

    initial commit

---
+++ src/main/java/fuck/json/JsonStuff/JsonUtilities.java
@@ -0,0 +1,98 @@
+package fuck.json.JsonStuff;
+
+import com.google.gson.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonUtilities {
+
+    private static final @NotNull Gson GSON;
+
+    static {
+        GSON = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    /**
+     * Convert a JSON String to a {@link Map}<{@link String}, {@link Object}>
+     * @param jsonString {@link String} in JSON format
+     * @return {@link Map}<{@link String}, {@link Object}>
+     */
+    public static @NotNull Map<String, Object> toMap(final @NotNull String jsonString) {
+        return toMap(GSON.fromJson(jsonString, JsonObject.class));
+    }
+
+    /**
+     * Convert a {@link JsonObject} to a {@link Map}<{@link String}, {@link Object}>
+     * @param jsonObject {@link JsonObject}
+     * @return {@link Map}<{@link String}, {@link Object}>
+     */
+    public static @NotNull Map<String, Object> toMap(final @NotNull JsonObject jsonObject) {
+        final Map<String, Object> map = new HashMap<>();
+        populateMap(jsonObject, map, "");
+        return map;
+    }
+
+    private static void populateMap(
+            final @NotNull JsonElement jsonElement,
+            final @NotNull Map<String, Object> map,
+            final @NotNull String prefix
+    ) {
+        if (jsonElement.isJsonArray()) {
+            final JsonArray jsonArray = jsonElement.getAsJsonArray();
+            final List<Object> list = new ArrayList<>();
+            jsonArray.forEach(element -> list.add(element.getAsString()));
+            map.put(prefix, list);
+            return;
+        }
+
+        if (!jsonElement.isJsonObject()) {
+            map.put(prefix, jsonElement.getAsString());
+            return;
+        }
+
+        final JsonObject jsonObject = jsonElement.getAsJsonObject();
+        for (final Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+            final String key = prefix.isEmpty() ? entry.getKey() : prefix + "." + entry.getKey();
+            populateMap(entry.getValue(), map, key);
+        }
+    }
+
+    /**
+     * Convert a {@link Map}<{@link String}, {@link Object}> to a JSON String
+     * @param map {@link Map}<{@link String}, {@link Object}>
+     * @return {@link String} in JSON format
+     */
+    public static @NotNull String toJsonString(final @NotNull Map<String, Object> map) {
+        final Map<String, Object> jsonMap = new HashMap<>();
+
+        map.forEach((key, value) -> {
+            final String[] keys = key.split("\\.");
+            populateJsonMap(jsonMap, keys, 0, value);
+        });
+
+        return GSON.toJson(jsonMap);
+    }
+
+    private static void populateJsonMap(
+            final @NotNull Map<String, Object> jsonMap,
+            final @NotNull String[] keys,
+            final int index,
+            final @NotNull Object value
+    ) {
+        if (index == keys.length - 1) {
+            jsonMap.put(keys[index], value);
+            return;
+        }
+
+        jsonMap.putIfAbsent(keys[index], new HashMap<String, Object>());
+        //noinspection unchecked
+        final Map<String, Object> map = (Map<String, Object>) jsonMap.get(keys[index]);
+
+        populateJsonMap(map, keys, index + 1, value);
+    }
+
+}
---
+++ src/main/java/fuck/json/Main.java
@@ -0,0 +1,31 @@
+package fuck.json;
+
+import fuck.json.JsonStuff.JsonConfig;
+import fuck.json.JsonStuff.JsonCreator;
+
+public class Main {
+
+    /**
+     * Funktioniert soweit :D
+     * Ich will den Code aber noch sotieren und erweitern.
+     * Code kommentieren
+     * Read Me hinzufügen.
+     * Eigenes Map System schreiben
+     */
+
+    public static void main(String[] args) {
+        System.out.println("--==#==--");
+
+        // JsonCreator jsonCreator = new JsonCreator("lel/config.json", "configs/config.json");
+        JsonCreator jsonCreator = new JsonCreator("configs/config.json");
+        JsonConfig config = jsonCreator.getConfig();
+        System.out.println(config.getString("botToken"));
+        config.set("botToken", "ohha");
+        System.out.println(config.getString("botToken"));
+        config.save();
+
+        System.out.println(config.getString("botToken", "not found"));
+        // WORKS!!!!! So far :DDD
+
+    }
+}
